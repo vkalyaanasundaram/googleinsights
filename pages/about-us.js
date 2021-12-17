bc216f9f357715e2b3056783ab3f0fc3a93042ac
Vinoth <vkalyaanasundaram@kapitus.com>
2021-12-17 17:34:43 +0530

    about us page our history modified with ACF row data

--- pages/about-us.js
+++ pages/about-us.js
@@ -28,14 +28,9 @@ const Footer = dynamic(() => import("../components/Footer"), {
 });
 export default function AboutUs() {
   const { asPath, pathname } = useRouter();
-  const [scrollY, setScrollY] = useState(0);
-  const [scroll, setScroll] = useState(false);
+  const elementRef = React.createRef();
   const [showHistory, setShowHistory] = useState(false);
-
-  const { observe, inView } = useInView({
-    onEnter: ({ unobserve }) => unobserve(), // only run once
-    onLeave: ({ observe }) => observe(),
-  });
+  // const [showEmp, setShowEmp] = useState(false);
 
   const { data, error } = useSWR(`/api/page/${asPath}`, fetcher);
 
@@ -53,20 +48,39 @@ export default function AboutUs() {
     },
   };
 
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollY(window.scrollY);
+  const { observe, inView } = useInView({
+    onEnter: ({ unobserve }) => unobserve(), // only run once
+    onLeave: ({ observe }) => observe(),
+  });
 
-      if (window.scrollY > 650) {
-        setShowHistory(true);
-      }
-      if (window.scrollY > 1500) {
-        setShowHistory(false);
-      }
-      window.addEventListener("scroll", () => {
-        setScroll(window.scrollY > 650);
-      });
-    };
+  const showEmployeeCont = () => {
+    if (window.scrollY >= 700) {
+      setShowHistory(false);
+    } else {
+      let employeeZero = document.querySelector("#employee-0");
+      let employeeOne = document.querySelector("#employee-1");
+      let employeeTwo = document.querySelector("#employee-2");
+      let employeeThree = document.querySelector("#employee-3");
+      let employeeFour = document.querySelector("#employee-4");
+      let employeeFive = document.querySelector("#employee-5");
+
+      setShowHistory(true);
+    }
+  };
+
+  useEffect(() => {
+    // const handleScroll = () => {
+    //   setScrollY(window.scrollY);
+    //   if (window.scrollY > 650) {
+    //     setShowHistory(true);
+    //   }
+    //   if (window.scrollY > 1500) {
+    //     setShowHistory(false);
+    //   }
+    //   window.addEventListener("scroll", () => {
+    //     setScrollY(window.scrollY > 650);
+    //   });
+    // };
   }, []);
 
   if (error) return <div>failed to load</div>;
@@ -80,23 +94,23 @@ export default function AboutUs() {
       </div>
       <h3 className="p-10 sticky">OUR HISTORY</h3>
 
-      <div className=" relative ">
+      {/* <div className=" relative ">
         <div className="flex my-20">
           <div className="flex-auto w-1/3 fixed top-1/3 ">
             {data?.aboutUs?.ourHistory.map((value, key) => (
               <div
                 key={key}
-                // className="leftHistory m-10"
+                className="leftHistory m-10"
                 className={scroll ? "active" : "inactive"}
                 id={"employee-" + key}
               >
-                {/* <ScrollSpy scrollThrottle={100} useBoxMethod={false}> */}
+                <ScrollSpy scrollThrottle={100} useBoxMethod={false}>
                 <div className="text-kapitus py-3">{value?.noOfEmployees}</div>
                 <hr />
                 <div className="text-kapitus py-3">{value?.fundedAmount}</div>
                 <hr />
                 <div className="text-kapitus py-3">{value?.businessFunded}</div>
-                {/* </ScrollSpy> */}
+                </ScrollSpy>
               </div>
             ))}
           </div>
@@ -127,6 +141,50 @@ export default function AboutUs() {
           </div>
           <div className="fixed-center">Scroll position: {scrollY}px</div>
         </div>
+      </div> */}
+      <div className="relative">
+        {data?.aboutUs?.ourHistoryRow.map((value, key) => (
+          <div key={key}>
+            <div className="flex my-20">
+              <div
+                className="ml-10 w-1/3 top-1/3 employeeContent "
+                id={"employee-" + key}
+              >
+                <div className="leftContent active-scroll-spy p-5">
+                  <div className="text-kapitus py-3">
+                    {value?.noOfEmployees}
+                  </div>
+                  <hr />
+                  <div className="text-kapitus py-3">{value?.fundedAmount}</div>
+                  <hr />
+                  <div className="text-kapitus py-3">
+                    {value?.businessFunded}
+                  </div>
+                </div>
+              </div>
+
+              <div className="w-1/2 businessContent" id={"content-" + key}>
+                <div className="flex">
+                  <div className="float-left text-kapitus text-3xl">
+                    {value?.companyYear}
+                  </div>
+                  <div className="float-left ml-10 ">
+                    <Image
+                      src={value?.svgIcon?.sourceUrl}
+                      width="100"
+                      alt=""
+                      height="100"
+                    />
+                  </div>
+                  <div className="float-left  ml-10 text-kapitus text-2xl">
+                    {value?.companyData}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <hr />
+          </div>
+        ))}
       </div>
 
       <div id="Teams">
@@ -137,8 +195,8 @@ export default function AboutUs() {
                 <a href={value?.profileImage?.sourceUrl}>
                   <Image
                     src={value?.profileImage?.sourceUrl}
-                    width="350"
-                    height="420"
+                    width="320"
+                    height="380"
                     alt=""
                     srl_gallery_image="true"
                   />
@@ -149,10 +207,12 @@ export default function AboutUs() {
         </SimpleReactLightbox>
       </div>
 
-      <div className="xs:w-full p-10 mt-10 mb-10 mx-auto">
+      <div className="float-left clear-both xs:w-full p-10 mt-10 mb-10 mx-auto">
         {ReactHtmlParser(data?.aboutUs?.footeContent)}
       </div>
-      <Footer />
+      <div className="float-left clear-both ">
+        <Footer />
+      </div>
     </>
   );
 }
